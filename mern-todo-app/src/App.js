6393e40ffbcdf65e1d2d8645de4bfa349636983d
jose marte <tumusicalatina@gmail.com>
2019-04-27 12:07:27 -0400

    Saturday class

---
+++ mern-todo-app/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return (
+      
+        <div className="container">
+          <h2>MERN-Stack Todo App</h2>
+        </div>
+      
+    );
+  }
+}
+
+export default App;
