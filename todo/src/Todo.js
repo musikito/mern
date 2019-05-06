44c4c4c5ff00dca6e7aca4e353981a1356610e48
jose marte <tumusicalatina@gmail.com>
2019-05-06 11:17:00 -0400

    Minor Updates

--- todo/src/Todo.js
+++ todo/src/Todo.js
@@ -6,22 +6,25 @@ class Todo extends Component {
      * It is mostly used to update the component when a user performs some action like clicking button, 
      * typing some text, pressing some key, etc.
      */
-    state = {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
         edit: false,
         id: null,
         mockData: [{
           id: '1',
-          title: 'Buy Milk',
+          title: 'Buy Milk.',
           done: false,
           date: new Date()
         }, {
           id: '2',
-          title: 'Meeting with Ali',
+          title: 'Meeting with Ali.',
           done: false,
           date: new Date()
         }, {
           id: '3',
-          title: 'Tea break',
+          title: 'Tea break.',
           done: false,
           date: new Date()
         }, {
@@ -31,10 +34,113 @@ class Todo extends Component {
           date: new Date()
         }]
       }
+    }
 
+
+
+
+
+    onSubmitHandle(event) {
+      event.preventDefault();
+  
+      this.setState({
+        mockData: [...this.state.mockData, {
+          id: Date.now(),
+          title: event.target.item.value,
+          done: false,
+          date: new Date()
+        }]
+      });
+  
+      event.target.item.value = '';
+    }
+  
+    onDeleteHandle() {
+      let id = arguments[0];
+  
+      this.setState({
+        
+        mockData: this.state.mockData.filter(item => {
+          if (item.id !== id) {
+            return item;
+          }
+        })
+      });
+    }
+  
+    onEditHandle(event) {
+      this.setState({
+        edit: true,
+        id: arguments[0],
+        title: arguments[1]
+      });
+    }
+  
+    onUpdateHandle(event) {
+      event.preventDefault();
+  
+      this.setState({
+        mockData: this.state.mockData.map(item => {
+          if (item.id === this.state.id) {
+            item['title'] = event.target.updatedItem.value;
+            return item;
+          }
+  
+          return item;
+        })
+      });
+  
+      this.setState({
+        edit: false
+      });
+    }
+  
+    onCompleteHandle() {
+      let id = arguments[0];
+  
+      this.setState({
+        mockData: this.state.mockData.map(item => {
+          if (item.id === id) {
+            item['done'] = true;
+            return item;
+          }
+  
+          return item;
+        })
+      });
+    }
+  
+    renderEditForm() {
+      if (this.state.edit) {
+        return <form onSubmit={this.onUpdateHandle.bind(this)}>
+          <input type="text" name="updatedItem" className="item" defaultValue={this.state.title} />
+          <button className="update-add-item">Update</button>
+        </form>
+      }
+    }
+    // eslint-disable-next-line no-useless-constructor
+    
 
     render() {
-        return(<h1>This Message is from Todo Component</h1>)
+      return (
+        <div>
+          {this.renderEditForm()}
+          <form onSubmit={this.onSubmitHandle.bind(this)}>
+            <input type="text" name="item" className="item" />
+            <button className="btn-add-item">Add</button>
+          </form>
+          <ul>
+            {this.state.mockData.map(item => (
+              <li key={item.id} className={ item.done ? 'done' : 'hidden' }>
+                {item.title}
+                <button onClick={this.onDeleteHandle.bind(this, item.id)}>Delete</button>
+                <button onClick={this.onEditHandle.bind(this, item.id, item.title)}>Edit</button>
+                <button onClick={this.onCompleteHandle.bind(this, item.id)}>Complete</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
     }
 }
 
